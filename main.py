f048c3ed09c14b55d10f0fa342a9cf9f8ec2ce74
Ishan Deshpande <deshpandeishan75@gmail.com>
2024-01-17 13:23:37 +0530

    Code for creating a dictionary is half done. Still facing one bug related to appendin dictionary value at the end oof previous value.

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ dictionary = CodeDictionary()
 limit = 11  # This variable holds the limit value of the total discounts allowed
 code_dict = {}
 for key in range(1, limit):
-    code = ""
     code = code_instance.get_code()
     code_dict = dictionary.create_dictionary(key, code)
 print(code_dict)
