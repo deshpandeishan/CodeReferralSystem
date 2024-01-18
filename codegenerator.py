9f4d81f13113ab1f12458112db923cd1a0787499
Ishan Deshpande <deshpandeishan75@gmail.com>
2024-01-18 11:12:47 +0530

    Optimised get_code method to reduce the code lines

--- codegenerator.py
+++ codegenerator.py
@@ -8,24 +8,16 @@ get_number = Number()
 class GenerateCode:
 
     def __init__(self):
-        self.code_list = []
-        self.letter_list = []
-        self.num_list = []
         self.code = ""
         self.letters = get_letter.str_to_list()
         self.numbers = get_number.return_numbers()
 
     def get_code(self):
-        self.letter_list = []
-        self.num_list = []
-        self.code_list = []
         self.code = ""
+        combined_list = []
         for _ in range(3):
-            self.letter_list.append(choice(self.letters))
-            self.num_list.append(choice(self.numbers))
-        self.code_list.extend(self.letter_list)
-        self.code_list.extend(self.num_list)
-        shuffle(self.code_list)
-        for _ in self.code_list:
-            self.code += _
+            combined_list.append(choice(self.letters))
+            combined_list.append(choice(self.numbers))
+        shuffle(combined_list)
+        self.code = ''.join(combined_list)
         return self.code
