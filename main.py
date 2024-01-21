4c94f5bd59ee8e2bd4e64cd0dc2be2ca2dbd3573
Ishan Deshpande <deshpandeishan75@gmail.com>
2024-01-21 19:21:48 +0530

    Fixed some minor bugs and modified the code.

--- main.py
+++ main.py
@@ -1,25 +1,6 @@
-from codegenerator import GenerateCode
-from Codelist import CodeList
-from user import InsertCode
-from code_manager import DiscountCode
+from user import InsertCode, CallCodeGenerator
 
-
-code_instance = GenerateCode()
-codelist = CodeList()
 insert_code = InsertCode()
-discountcode = DiscountCode()
-
-limit = 11  # This variable holds the limit value of the total discounts allowed for an event
-code_list = []
-for _ in range(1, limit):
-    code = code_instance.get_code()
-    code_list = codelist.create_list(code)
-
-print(f"All codes:  {code_list}")
-
-condition = True
-while condition:
-    user_input = insert_code.take_user_ip()
-    insert_code.manage_codes(user_input, code_list)
-    if user_input == "6875":
-        condition = False
+generate_code = CallCodeGenerator()
+generate_code.call_code()
+generate_code.task()
--- user.py
+++ user.py
@@ -1,8 +1,13 @@
 from code_manager import DiscountCode
 from discount import Discount
+from codegenerator import GenerateCode
+from Codelist import CodeList
 
 discount_code = DiscountCode()
 discount = Discount()
+code_generator = GenerateCode()  # Corrected variable name
+codelist = CodeList()
+
 
 class InsertCode:
 
@@ -10,7 +15,6 @@ class InsertCode:
         self.user_input = ''
         self.used_code_list = []
 
-    # take_ui means take user input
     def take_user_ip(self):
         self.user_input = input("Enter your discount coupon code here:   ")
         return self.user_input
@@ -22,5 +26,31 @@ class InsertCode:
             else:
                 self.used_code_list.extend(discount_code.insert_used_code(user_input))
                 print(f"Your discounted price is:    {discount.calculate_discount()}")
+                refer = int(input("Type '1' for referring to the friend else type '0':    "))
+                if refer == 1:
+                    pass    # Add method to display any code except the used code.
         else:
             print("Error! Invalid code")
+
+
+class CallCodeGenerator(InsertCode):
+
+    def __init__(self):
+        super().__init__()
+        self.limit = 11
+        self.code_list = []
+        self.code_str = ""
+        self.condition = True
+
+    def call_code(self):
+        for _ in range(0, self.limit):
+            self.code_str = code_generator.get_code()  # Corrected method call
+            self.code_list.append(self.code_str)
+        print(self.code_list)
+
+    def task(self):
+        while self.condition:
+            user_input = self.take_user_ip()
+            self.manage_codes(user_input, self.code_list)
+            if user_input == "6875":
+                self.condition = False
